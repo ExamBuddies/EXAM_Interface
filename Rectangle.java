6396c1962f2c96652e275c5f9100604b73e0f832
DAT14V1 - Casper Vejdiksen <casp539b@stud.kea.dk>
2015-05-30 16:24:14 +0200

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,16 @@
+// Represents rectangular shapes.
+public class Rectangle implements Shape {
+	private double width;
+	private double height;
+
+	// constructs a new rectangle with the given dimensions
+	public Rectangle(double width, double height) {
+		this.width = width;
+		this.height = height;
+	}
+	// returns the area of this rectangle
+	public double getArea() {
+		return width * height;
+	}
+
+}
